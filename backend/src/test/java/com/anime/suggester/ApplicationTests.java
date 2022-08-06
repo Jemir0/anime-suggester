f4b3c934ac8739d2c32f629b187a9abe769b1b96
Resci <vIsUAl123>
2022-08-07 01:20:03 +0300

    Init backend

---
+++ backend/src/test/java/com/anime/suggester/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.suggester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
